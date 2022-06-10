5d109af4db89803ea5021002fcadfea9aec7b634
lucky <541235842@qq.com>
2022-06-10 20:51:00 +0800

    fix(优化): 优化一直问题

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import "@/style/index.scss"
+import '@/style/index.scss'
 import App from './App.vue'
 import router from '@/router'
-import {store,key} from "./store"
+import { store, key } from './store'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-
+//初始化
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
-
-app.use(ElementPlus).use(router as any).use(store as any,key)
+app
+  .use(ElementPlus)
+  .use(router as any)
+  .use(store as any, key)
 app.mount('#app')
